425dc9555787aea9037650fefa590d5fd0eaf811
zly <yun3948@163.com>
2023-11-28 23:32:48 +0800

    修改密碼字符長度

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -13,17 +13,17 @@
 
             <div class="block">
                 <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" readonly class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
+                <x-input  id="email" readonly class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
             </div>
 
             <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                <x-label for="password" value="新密碼 (最少 8 個字元) " />
+                <x-input minlength="8" id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
             </div>
 
             <div class="mt-4">
-                <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+                <x-label for="password_confirmation" value="確認新密碼 (最少 8 個字元) " />
+                <x-input minlength="8" id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
             </div>
 
             <div class="flex items-center justify-end mt-4">
--- resources/views/user/member_password.blade.php
+++ resources/views/user/member_password.blade.php
@@ -35,12 +35,12 @@
 
             <div class="form-group pb-3 mt-4">
                 <label for="password">新密碼 (最少 8 個字元) </label>
-                <input type="password" class="form-control" required value="" name="password" id="password" placeholder="新密碼">
+                <input minlength="8" type="password" class="form-control" required value="" name="password" id="password" placeholder="新密碼">
             </div>
 
             <div class="form-group pb-3 mt-4">
                 <label for="password_confirmation">確認新密碼(最少 8 個字元)</label>
-                <input type="password" class="form-control" required value="" name="password_confirmation"
+                <input minlength="8" type="password" class="form-control" required value="" name="password_confirmation"
                     id="password_confirmation" placeholder="確認新密碼(最少 8 個字元)">
             </div>
 
